d19a6116351ae668553e4da483a44d48342246a0
Piotr Sampławski <sampi@gumed.edu.pl>
2015-01-22 12:02:59 +0100

    tell ember-rails which version of ember-js to use in each environment
    the production version is minified and not allows for logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # ember.js
+  config.ember.variant = :development
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # ember.js
+  config.ember.variant = :production
 end
